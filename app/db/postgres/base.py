27e6f1dbd9c188453e2329aa2250bbeb8c06d3d8
Dmitrii Annsjaw <rotanin@gmail.com>
2024-06-16 16:48:29 +0500

    add role repo

---
+++ app/db/postgres/base.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from app.core.config import app_settings
+
+
+async_engine = create_async_engine(url=app_settings.POSTGRES_DSN, echo=False)
+async_session = async_sessionmaker(bind=async_engine, expire_on_commit=False, class_=AsyncSession)
