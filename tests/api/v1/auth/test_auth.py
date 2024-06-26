5fe7346bc1695ceb136b9b425b486a2bd8ae3988
Носов Сергей Олегович <SONosov@datafort.ru>
2024-06-26 21:46:15 +0300

    Update

--- tests/api/v1/auth/test_auth.py
+++ tests/api/v1/auth/test_auth.py
@@ -1,6 +1,8 @@
 import pytest
 from httpx import AsyncClient
+from fastapi import status
 
+from app.exceptions import user_already_exists_error
 from app.main import app
 from app.schemas.api.v1.auth_schemas import (
     RegisterResponseSchema,
@@ -23,5 +25,22 @@ class TestAuthentication:
         response_json = response.json()
 
         # Assert
+        assert response.status_code == status.HTTP_201_CREATED
         assert response_json == RegisterResponseSchema(**response_json).model_dump(mode='json')
         assert await user_repository.get(user_id=response_json['id'])
+
+    async def test_register_409_username_duplicate(self, async_test_client: AsyncClient):
+        # Arrange
+        user_data = RegisterUserCredentialsSchema(
+            username='random_username', email='another@email.com', password='random_password'
+        )
+        user_data_json = user_data.model_dump(mode='json')
+        await async_test_client.post(app.url_path_for('api_v1_register'), json=user_data_json)
+
+        # Act
+        response = await async_test_client.post(app.url_path_for('api_v1_register'), json=user_data_json)
+        response_json = response.json()
+
+        # Assert
+        assert response.status_code == status.HTTP_409_CONFLICT
+        assert response_json == {'detail': user_already_exists_error.detail}
